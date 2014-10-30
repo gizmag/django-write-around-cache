28cf8f9c4ad7e82f88936c4e461eaebc1a698d0d
Jacob Haslehurst <jacob@haslehurst.net>
2014-10-30 16:08:42 +1100

    add in simple view mixin

---
+++ django_write_around_cache/view_mixins.py
@@ -0,0 +1,7 @@
+class WriteAroundCacheMixin(object):
+    self.cache_mode = 'standard'
+
+    def get_context_data(self):
+        context = super(WriteAroundCacheMixin, self).get_context_data()
+        context['cache_mode'] = self.cache_mode
+        return context
