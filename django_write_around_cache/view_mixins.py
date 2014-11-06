b3f6a06c39fc937842eea614b226749ce2bb64d6
Jacob Haslehurst <jacob@haslehurst.net>
2014-11-06 15:27:49 +1100

    pass through get_context_data args/kwargs

--- django_write_around_cache/view_mixins.py
+++ django_write_around_cache/view_mixins.py
@@ -1,7 +1,7 @@
 class WriteAroundCacheMixin(object):
     cache_mode = 'standard'
 
-    def get_context_data(self):
-        context = super(WriteAroundCacheMixin, self).get_context_data()
+    def get_context_data(self, *args, **kwargs):
+        context = super(WriteAroundCacheMixin, self).get_context_data(*args, **kwargs)
         context['cache_mode'] = self.cache_mode
         return context
